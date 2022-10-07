868620bdbb30f3495fbdc2f154f8083e99533146
LottaMonet <lotta@anniecannons.com>
2022-10-06 18:58:44 -0700

    updated submit and added useCallback

--- src/Pages/YourMovies.js
+++ src/Pages/YourMovies.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import MovieCard from "../Components/MovieCard";
@@ -11,6 +11,11 @@ const YourMovies = () => {
     
     const [searchMovie, setSearchMovie] = useState({movieSearch: ''});
     const [movieData, setMovieData] = useState(data);
+    const [pressed, setPressed] = useState(false);
+
+    function handlePressed() {
+        setPressed(true);
+    }
 
     const handleSearchMovie = (e) => {
         const value = e.target.value;
@@ -19,27 +24,34 @@ const YourMovies = () => {
     };
 
     
+        const getMovie = useCallback(async (title) => {
+            title = searchMovie.movieSearch;
+            let response = await fetch(`https://www.omdbapi.com/?apikey=11448ed6&t=${title}}`);  
+            let movieData = await response.json();
+            console.log(movieData, 'test movieData');
+            let movieDataArr = [];
+            movieDataArr.push(movieData)
+            console.log(movieDataArr, 'test movie data array');
+            setMovieData(movieDataArr);
+            handlePressed();
+        }, [searchMovie.movieSearch]);
+
+    
+
+
   useEffect(() => {
-    async function getMovie(title, i) {
-        title = searchMovie.movieSearch;
-         let response = await fetch(`http://www.omdbapi.com/?apikey=11448ed6&t=${title}}`);  
-        let movieData = await response.json();
-        console.log(movieData, 'test movieData');
-        let movieDataArr = [];
-        movieDataArr.push(movieData)
-        console.log(movieDataArr, 'test movie data array');
-        setMovieData(movieDataArr);
+    if (pressed === true) {
+    getMovie();
     }
-    getMovie(searchMovie.movieSearch)
-  }, []);
+  }, [getMovie, pressed]);
 
     return ( 
         <div id="body">
             <Header title1 = "Your " title2="Movies" />
             <div id="search-section">
                 <label htmlFor="search" id="search-label" >Search</label><br/>
-                <input type="search" name="movieSearch" id="movie"  value={searchMovie.movieSearch} onChange={e => handleSearchMovie(e)} /><br/>
-                <button type="submit" id="search-btn" onClick={getMovie}>Search</button>
+                <input type="search" name="movieSearch" id="movie"  value={searchMovie.movieSearch}  onChange={e => handleSearchMovie(e)} /><br/>
+                <button type="submit" id="search-btn" onClick={getMovie} >Search</button>
             </div>
             <div id="movie-card-section">
                 {
